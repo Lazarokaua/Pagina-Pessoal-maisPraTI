37401c462ea9275a2a9571b4f79f104301c43ccb
Lazarokaua <lazarokaua4@gmail.com>
2025-07-19 16:43:58 -0300

    feat: effects on cardProject!

--- src/components/layout/ProjectCard.tsx
+++ src/components/layout/ProjectCard.tsx
@@ -10,14 +10,14 @@ interface ProjectCardProps {
 
 export function ProjectCard({ url, srcImg, titleProject, descriptionProject, mobileAvaible }: ProjectCardProps) {
     return (
-        <div className="grid grid-cols-1 px-6 py-4 text-slate-600">
+        <div className="grid grid-cols-1 px-6 py-4 text-slate-600 group">
             <a href={url} target="_blank">
                 <Image
                     src={srcImg}
                     alt={titleProject}
                     width={384}
                     height={176}
-                    className="w-full max-w-md h-44 sm:h-56 object-cover cursor-pointer rounded-lg transition-all duration-300"
+                    className="w-full max-w-md h-44 sm:h-56 object-cover cursor-pointer rounded-lg transition-transform duration-300 group-hover:scale-105"
                     priority
                 />
             </a>
